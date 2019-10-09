f16cfe54b1dc77281df48043d498495ce3c309da
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-09 13:46:58 +0200

    Added the routes for the pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/categories">Categories</router-link>
+      <router-link to="/random">Random</router-link>
     </div>
     <router-view />
   </div>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Categories from "./views/Categories.vue";
+import Random from "./views/Random.vue";
+
+// Probably don't need a Random page, just a detail page with a random button
 
 Vue.use(Router);
 
@@ -12,6 +16,16 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/categories",
+      name: "categories",
+      component: Categories,
+    },
+    {
+      path: "/random",
+      name: "random",
+      component: Random,
     },
   ],
 });
