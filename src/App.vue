d973bc8373f127bead3a77e2deaa32ab4f66777b
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-14 17:33:46 +0200

    You can now sort the table by clicking on the headers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <SortableTable v-on:sort-table="sortBy" v-bind:users="users" />
+  </div>
 </template>
 
 <script>
+import SortableTable from "./components/sortable-table.vue";
+
 export default {
   name: "app",
+  components: {
+    SortableTable
+  },
   data() {
     return {
-      users: undefined
+      users: [],
+      sortingKey: undefined,
+      descending: false
     };
   },
   beforeMount() {
@@ -19,9 +28,48 @@ export default {
         `https://randomuser.me/api/?results=${amount}`
       );
       const { info, results } = await response.json();
-      console.log(results);
 
-      this.users = results;
+      this.usersData = this.cleanData(results);
+    },
+
+    sortBy({ key, type }) {
+      if (type === "string") {
+        this.users = this.users.sort(function(a, b) {
+          if (a[key] < b[key]) {
+            return -1;
+          } else if (a[key] > b[key]) {
+            return 1;
+          }
+
+          return 0;
+        });
+      }
+
+      if (type === "number") {
+        this.users = this.users.sort(function(a, b) {
+          return a[key] - b[key];
+        });
+      }
+
+      if (key === this.sortingKey) {
+        this.descending = !this.descending;
+
+        if (this.descending) {
+          this.users = this.users.reverse();
+        }
+      }
+
+      this.sortingKey = key;
+    },
+    cleanData(data) {
+      this.users = data.map(function(user) {
+        return {
+          age: user.dob.age,
+          firstName: user.name.first,
+          lastName: user.name.last,
+          country: user.location.country
+        };
+      });
     }
   }
 };
