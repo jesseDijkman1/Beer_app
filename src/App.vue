9ce7a7147b9ec548894ee157d6b99df75fda1a5b
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-14 14:48:04 +0200

    Can make a request to a random user api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <div id="app">
-    <h1>Hello</h1>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      users: undefined
+    };
+  },
+  beforeMount() {
+    this.getUsers(10);
+  },
+  methods: {
+    async getUsers(amount = 1) {
+      const response = await fetch(
+        `https://randomuser.me/api/?results=${amount}`
+      );
+      const { info, results } = await response.json();
+      console.log(results);
+
+      this.users = results;
+    }
+  }
 };
 </script>
 
