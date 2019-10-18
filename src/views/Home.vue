c01783a88308bf6b42fd8fda64b1496969fb0a45
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-18 10:55:53 +0200

    Removed the placeholder files and code. Added some configuration

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: "home",
+  methods: {
+    test() {
+      console.log("boofoawef");
+    },
+  },
+};
+</script>
