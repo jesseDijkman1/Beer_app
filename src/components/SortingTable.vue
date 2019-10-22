7b637d7be23b1273a9163b6643c7e6d8065a43c6
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-22 16:31:23 +0200

    Added SortingTable.vue for displaying the 10 records and sorting them

---
+++ src/components/SortingTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <table>
+    <tr>
+      <th v-bind:key="id" v-for="(n, id) in Object.keys(dataArray[0])">{{n}}</th>
+    </tr>
+    <tr v-bind:key="beer.id" v-for="beer in dataArray">
+      <td>{{beer.id}}</td>
+      <td>{{beer.name}}</td>
+      <td>{{beer.description}}</td>
+    </tr>
+  </table>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch, Vue } from "vue-property-decorator";
+
+@Component
+export default class SortingTable extends Vue {
+  @Prop() dataArray!: any[];
+}
+</script>
