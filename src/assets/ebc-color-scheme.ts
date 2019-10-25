2d6a29ddbf6c59d53f95c3510185d12ebe79fc1b
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-25 09:59:22 +0200

    Added a file that contains information about the colors beers should have based on their ebc value, this will be used by a color scaler

---
+++ src/assets/ebc-color-scheme.ts
@@ -0,0 +1,58 @@
+export default [
+  {
+    value: 4,
+    color: [248, 247, 83]
+  },
+  {
+    value: 6,
+    color: [246, 245, 19]
+  },
+  {
+    value: 8,
+    color: [236, 230, 26]
+  },
+  {
+    value: 12,
+    color: [213, 188, 38]
+  },
+  {
+    value: 16,
+    color: [191, 146, 59]
+  },
+  {
+    value: 20,
+    color: [191, 129, 58]
+  },
+  {
+    value: 26,
+    color: [188, 103, 51]
+  },
+  {
+    value: 33,
+    color: [141, 76, 50]
+  },
+  {
+    value: 39,
+    color: [93, 52, 26]
+  },
+  {
+    value: 47,
+    color: [38, 23, 22]
+  },
+  {
+    value: 57,
+    color: [15, 11, 10]
+  },
+  {
+    value: 69,
+    color: [8, 7, 7]
+  },
+  {
+    value: 79,
+    color: [3, 4, 3]
+  },
+  {
+    value: 138,
+    color: [0, 0, 0]
+  }
+]
