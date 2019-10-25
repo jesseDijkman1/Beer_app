e58599fb6ce4c0585f85efafd9146fa943957492
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-25 10:16:33 +0200

    Added a ColorScheme interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,15 @@ export interface BeerData {
   id: number;
   name: string;
   description: string;
+  ebc: number;
 }
 
 export interface RouteObject {
   name: string;
   path: string;
+}
+
+export interface ColorScheme {
+  color: number[];
+  value: number;
 }
