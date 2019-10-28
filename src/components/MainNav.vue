e4f4cdb3c8fda500b59c4d44276d3381638f3882
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-28 12:30:18 +0100

    Added a random router-link, might put this in a component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home!</router-link>
     <router-link to="/about">About</router-link>
+    <router-link @click.native="randomBeer = newRandomBeer()" :to="`/beers/${randomBeer}`">Random</router-link>
   </nav>
 </template>
 
@@ -9,5 +10,11 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class MainNav extends Vue {}
+export default class MainNav extends Vue {
+  randomBeer: number = this.newRandomBeer();
+
+  newRandomBeer() {
+    return Math.floor(Math.random() * 325) + 1;
+  }
+}
 </script>
