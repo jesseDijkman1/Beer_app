021dcdec17699ae5589ff4d03d8dea4026ba3820
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-28 15:54:53 +0100

    Added a list view for displaying all the beers

---
+++ src/views/List.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Watch, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
