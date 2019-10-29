7f29553f80624f892d8c3d33036b2c46fd6edb90
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-29 10:27:08 +0100

    Created a color display component for the ebc values

---
+++ src/components/EbcColorDisplay.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="color-display" :style="{background: `rgb(${getColorFromEbc(ebc)})`}"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import colorFromValue from "@/modules/color-scaler.ts";
+import ebcColorScheme from "@/assets/ebc-color-scheme.ts";
+
+@Component
+export default class EbcColorDisplay extends Vue {
+  @Prop() ebc!: number;
+
+  getColorFromEbc: Function = colorFromValue(ebcColorScheme);
+}
+</script>
+
+<style lang="scss" scoped>
+.color-display {
+  width: 30px;
+  height: 30px;
+  border-radius: 100px;
+}
+</style>
