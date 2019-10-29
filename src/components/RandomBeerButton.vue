450c45638699a11bee9bc4fca0a2e0964b2317c7
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-29 10:52:40 +0100

    Created a randomBeerbutton, so you can now get a random beer from a link and button

---
+++ src/components/RandomBeerButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="$router.push({name: 'detail', params: { id: randomId(), hasRandomId: true }})">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class RandomBeerButton extends Vue {
+  randomId() {
+    return Math.floor(Math.random() * 325) + 1;
+  }
+}
+</script>
