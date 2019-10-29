4da923306b4ce3782cf9ea1fce18cde48b0d7904
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-29 09:18:36 +0100

    Created a randombeerlink component that generates a new random id on a click and sends the user to the detail page

---
+++ src/components/RandomBeerLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link @click.native="setRandomId" :to="{name: 'random', params: { id }}">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class RandomBeerLink extends Vue {
+  id!: number;
+
+  setRandomId() {
+    this.id = Math.floor(Math.random() * 325) + 1;
+  }
+}
+</script>
