53f019f9d557942e843098923b9bc533df15ada4
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-29 12:02:10 +0100

    Added a footerpagination component

---
+++ src/components/FooterPagination.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer></footer>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class FooterPagination extends Vue {
+  @Prop() perPage!: number;
+  @Prop() totalEntries!: number;
+  @Prop({ default: null }) max!: number | null;
+
+  totalPages: number = Math.ceil(this.totalEntries / this.perPage);
+
+  created() {
+    console.log("ok", this.totalPages);
+  }
+}
+</script>
