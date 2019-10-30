b9d5943b923fc25f3fdddc732e8d5ebc5947538e
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-30 14:10:13 +0100

    Added a container component for the articles

---
+++ src/components/ArticlesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="articles-list">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ArticlesList extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.articles-list {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-row-gap: 3em;
+}
+</style>
