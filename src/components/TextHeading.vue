64a5eef381d80021699b3370eb3bc5a1ac10a98d
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-30 09:38:18 +0100

    Added a heading component that takes a size prop

---
+++ src/components/TextHeading.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 :class="'heading--' + size">
+    <slot></slot>
+  </h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+type sizes = "large" | "medium" | "small";
+
+@Component
+export default class TextHeading extends Vue {
+  @Prop() size: sizes = "large";
+}
+</script>
+
+<style lang="scss" scoped>
+.heading--large {
+  font-size: 42px;
+}
+
+.heading--medium {
+  font-size: 32px;
+}
+
+.heading--small {
+  font-size: 24px;
+}
+</style>
