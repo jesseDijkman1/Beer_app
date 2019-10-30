cc152c22843948ac936fde0479bd1b33838fa3e2
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-30 12:00:49 +0100

    Added BeerArticleCard for displaying the beers in a list, I will use this over the table, because styling the table is a pain

---
+++ src/components/BeerArticleCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="beer-article">
+    <span class="beer-article__color"></span>
+    <div class="image-container">
+      <img :src="img" alt />
+    </div>
+    <h1 class="beer-article__title">{{name}}</h1>
+    <p class="beer-article__tagline">{{tagline}}</p>
+    <strong class="beer-article__alcohol">{{alcohol}}</strong>
+  </article>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import EbcColorDisplay from "@/components/EbcColorDisplay.vue";
+
+@Component({
+  components: {
+    EbcColorDisplay,
+  },
+})
+export default class BeerArticleCard extends Vue {
+  @Prop({ default: "" }) name!: string;
+  @Prop({ default: "" }) tagline!: string;
+  @Prop() alcohol!: number;
+  @Prop() img!: string;
+  @Prop() ebc!: number;
+}
+</script>
