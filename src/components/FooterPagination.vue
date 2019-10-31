a1c10badc6efc71c07a7b15452ab9f013db2745e
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-31 14:20:57 +0100

    Added a next and previous button to the pagination, and styled the component

--- src/components/FooterPagination.vue
+++ src/components/FooterPagination.vue
@@ -1,10 +1,25 @@
 <template>
-  <footer>
-    <ol>
-      <li :key="n" v-for="n in pages">
-        <router-link :class="{'is-active': n == currentPage}" :to="url + n">{{n}}</router-link>
+  <footer class="footer-pagination">
+    <button
+      :class="{'is-hidden': Number(currentPage) == 1}"
+      @click="previousPage"
+      class="footer-pagination__button footer-pagination__button--back"
+    ></button>
+    <ol class="footer-pagination__list">
+      <li
+        class="footer-pagination__page"
+        :key="n"
+        v-for="n in pages"
+        :class="{'is-active': n == currentPage}"
+      >
+        <router-link :to="url + n">{{n}}</router-link>
       </li>
     </ol>
+    <button
+      :class="{'is-hidden': Number(currentPage) == totalPages}"
+      @click="nextPage"
+      class="footer-pagination__button footer-pagination__button--next"
+    ></button>
   </footer>
 </template>
 
@@ -47,5 +62,80 @@ export default class FooterPagination extends Vue {
 
     return array.sort((a, b) => a - b);
   }
+
+  previousPage() {
+    const current = Number(this.currentPage);
+
+    if (current > 1) {
+      this.$router.push(this.url + (current - 1));
+    }
+  }
+
+  nextPage() {
+    const current = Number(this.currentPage);
+
+    if (current < this.totalPages) {
+      this.$router.push(this.url + (current + 1));
+    }
+  }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.footer-pagination {
+  border-top: solid 2px var(--color-main);
+  padding-top: 0.5em;
+  display: flex;
+  // justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+
+  &__button {
+    background: none;
+    border: none;
+    font-size: inherit;
+    height: 1em;
+    width: 1em;
+    background: white;
+    opacity: 0.5;
+    transition: opacity 0.2s ease;
+
+    &--next {
+      clip-path: polygon(0% 0%, 100% 50%, 0% 100%);
+      margin-left: auto;
+    }
+
+    &--back {
+      clip-path: polygon(100% 0%, 0% 50%, 100% 100%);
+      margin-right: auto;
+    }
+
+    &:hover {
+      cursor: pointer;
+      opacity: 1;
+    }
+  }
+
+  &__list {
+    display: grid;
+    grid-auto-flow: column;
+    grid-gap: 1em;
+  }
+
+  &__page {
+    position: relative;
+
+    &.is-active {
+      color: var(--color-main);
+      text-decoration: underline;
+      font-weight: bold;
+    }
+  }
+}
+
+.is-hidden {
+  pointer-events: none;
+  opacity: 0;
+}
+</style>
