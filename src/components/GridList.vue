1e76889c5286dcd1538de2674e22d1bbafdce4ad
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-10-31 08:47:10 +0100

    Added a component that turns passed in elements into a grid list

---
+++ src/components/GridList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="articles-list">
+    <slot></slot>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class GridList extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.articles-list {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-row-gap: 3em;
+}
+</style>
