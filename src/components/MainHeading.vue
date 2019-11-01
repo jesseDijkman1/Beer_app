1f49b63eb32fc34bcee58ac3e7ff265c457131a8
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-11-01 11:51:31 +0100

    Created MainHeading component

---
+++ src/components/MainHeading.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="main-heading">
+    <slot></slot>
+  </h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.main-heading {
+  color: var(--color-main);
+  font-family: Calibri, Arial, sans-serif;
+  font-size: 2.75rem;
+}
+</style>
