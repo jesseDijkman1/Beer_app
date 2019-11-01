d2b90513d2ad8d18cdf218133308fe711e1a8f51
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-11-01 11:27:02 +0100

    Added styling to UnitSymbol.vue

--- src/components/UnitSymbol.vue
+++ src/components/UnitSymbol.vue
@@ -1,16 +1,37 @@
 <template>
-  <span>{{unitSymbol}}</span>
+  <span class="unit-symbol">{{unitSymbol}}</span>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
-type units = "percentage" | "litres" | "kilograms" | "grams" | "celsius";
-
 @Component
 export default class extends Vue {
-  @Prop() unit!: string;
+  @Prop({ default: "" }) unit!: string;
 
-  get unitSymbol() {}
+  get unitSymbol() {
+    switch (this.unit) {
+      case "percentage":
+        return "%";
+      case "litres":
+        return "l";
+      case "kilograms":
+        return "kg";
+      case "grams":
+        return "g";
+      case "celsius":
+        return "\xB0C";
+      default:
+        return "";
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.unit-symbol {
+  font-size: 0.4em;
+  align-self: flex-end;
+  margin-bottom: 0.5em;
+}
+</style>
