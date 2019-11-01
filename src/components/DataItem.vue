83d65f01d703764da8edcf1a51da8236d3b5a1e3
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-11-01 10:49:25 +0100

    Created DataItem component

---
+++ src/components/DataItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <dl class="data-item">
+    <dt class="data-item__title"></dt>
+    <dd class="data-item__value">
+      <span class="data-item__unit"></span>
+    </dd>
+  </dl>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  @Prop() name!: string;
+  @Prop() value!: number | number;
+  @Prop() unit!: string;
+}
+</script>
