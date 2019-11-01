726618b85cded4a0499d59cc691ef6cfa46be3e1
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-11-01 10:49:40 +0100

    Created unitSymbol component

---
+++ src/components/UnitSymbol.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>{{unitSymbol}}</span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+type units = "percentage" | "litres" | "kilograms" | "grams" | "celsius";
+
+@Component
+export default class extends Vue {
+  @Prop() unit!: string;
+
+  get unitSymbol() {}
+}
+</script>
