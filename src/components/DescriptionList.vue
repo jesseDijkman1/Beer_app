74bd7f5be4916433cae0a72287b23f437eaf8ae3
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-11-02 17:07:42 +0100

    Created DescriptionList component

---
+++ src/components/DescriptionList.vue
@@ -0,0 +1,21 @@
+<template>
+  <dl>
+    <template>
+      <dt></dt>
+      <dd></dd>
+    </template>
+  </dl>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  @Prop() name!: string;
+  @Prop() value!: string | number;
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
