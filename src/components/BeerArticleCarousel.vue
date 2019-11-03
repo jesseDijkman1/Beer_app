0876b04d38d0815a075b0523106441a0deda7e5d
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-11-03 20:10:49 +0100

    Added a BeerArticleCarousel component

---
+++ src/components/BeerArticleCarousel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="article-carousel">
+    <div class="article-carousel__container">
+      <div class="article-carousel__articles">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.article-carousel {
+  &__container {
+    max-width: 1300px;
+    margin: 0 auto;
+    padding: 0 0.5rem;
+  }
+
+  &__articles {
+    grid-column: 2 / 3;
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: 1fr;
+    grid-gap: 3em;
+    overflow: scroll;
+  }
+}
+</style>
